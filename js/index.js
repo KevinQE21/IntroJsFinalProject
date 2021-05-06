70b66ddd861828ed18f88bd91f636f7d3f42075b
Kevin Quesada <kevinque01@gmail.com>
2021-05-05 21:12:14 -0600

    Funcion de limpiar los datos, junto con nuevos colores del webpage

--- js/index.js
+++ js/index.js
@@ -1,26 +1,24 @@
 function init() {
-  const recipe = document.getElementById("food-recipe").value;
+  const recipe = document.getElementById('food-recipe').value;
 
   if (!recipe) return insertError();
-
+  cleanData();
   return getData(recipe);
 }
 
 function insertError() {
-  const error = document.getElementById("food-recipe");
-  error.className = "error";
+  const error = document.getElementById('food-recipe');
+  error.className = 'error';
 }
 
 function getData(recipe) {
-  const APP_ID = "35c6ca2e";
-  const APP_KEY = "9e890929f8898cc7ce56be8d77bc8666";
+  const APP_ID = '35c6ca2e';
+  const APP_KEY = '9e890929f8898cc7ce56be8d77bc8666';
   const url = `https://api.edamam.com/search?q=${recipe}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   fetch(url)
     .then((response) => response.json())
-    .then((results) => {
-      handleData(results);
-    })
+    .then((results) => handleData(results))
     .catch((err) => console.log(err));
 }
 
@@ -37,19 +35,19 @@ function handleData({ hits }) {
 function displayInfo(recipe) {
   const { label, url, image, calories } = recipe;
 
-  const infoRecipes = document.getElementById("info-recipes");
+  const infoRecipes = document.getElementById('info-recipes');
 
-  let recipesCards = document.createElement("div");
-  recipesCards.className = "recipe";
+  let recipesCards = document.createElement('div');
+  recipesCards.className = 'recipe';
 
-  let recipeTittle = document.createElement("div");
-  recipeTittle.className = "recipe-tittle";
+  let recipeTittle = document.createElement('div');
+  recipeTittle.className = 'recipe-tittle';
   recipeTittle.innerHTML = `<h2>${label}</h2>
-                            <p>Calories: ${calories}</p>
-                            <a href="${url}">Recipe Url</a>`;
+                            <p>Calories: ${Math.round(calories)}</p>
+                            <a href="${url} target="_blank">Recipe Url</a>`;
 
-  let recipeImg = document.createElement("div");
-  recipeImg.className = "recipe-img";
+  let recipeImg = document.createElement('div');
+  recipeImg.className = 'recipe-img';
   recipeImg.innerHTML = `<img src="${image}" alt="${label}"></img>`;
 
   recipesCards.appendChild(recipeTittle);
@@ -57,9 +55,16 @@ function displayInfo(recipe) {
 
   infoRecipes.appendChild(recipesCards);
 
-  document.getElementById("food-recipe").value = "";
+  document.getElementById('food-recipe').value = '';
 }
 
 function showDataError() {}
 
+function cleanData() {
+  const infoRecipe = document.getElementById('info-recipes');
+  infoRecipe.innerHTML = '';
+}
 // calories, image, label, url
+
+const button = document.getElementById('send_info');
+button.addEventListener('click', init);
